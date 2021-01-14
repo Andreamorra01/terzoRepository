c1ebadd9445e34bdf597b2bb1316a0dea0f54aee
Andrea Morra <andreamorra01@gmail.com>
2021-01-14 15:23:33 +0100

    verifica metodo

--- index.php
+++ index.php
@@ -8,3 +8,12 @@ $link = mysqli_connect("db", "root", "pass");
     
     
     echo "Connect Successfully. Host info: " . mysqli_get_host_info($link);
+    $andrea = "ciao";
+    echo $andrea;
+    if($_SERVER['SERVER_METHOD'] === 'GET'){
+        echo "il metodo usato è GET";
+    }
+    else {
+        echo "il metodo usato non è GET";
+    }
+    
